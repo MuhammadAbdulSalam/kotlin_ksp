f9973e604cb39dc3519da0f9f87036672952546c
Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>
2023-07-13 23:03:32 +0100

    working plugin

--- buildSrc/src/main/java/SnapIGradlePlugin.kt
+++ buildSrc/src/main/java/SnapIGradlePlugin.kt
@@ -8,8 +8,29 @@ class SnapItPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         project.tasks.register<Copy>("snapItGenerate") {
+
             dependsOn("assemble")
-            val packagePath = File("${project.buildDir}/snapIt", "snap_config.txt").readText()
+
+            val snapConfigFile = File("${project.buildDir}/snapIt", "snap_config.txt")
+
+            if (!snapConfigFile.exists()) {
+                throw (Exception(
+                    """property snapItTestDestination { <TEST SOURCESET LOCATION> } missing from module build.gradle
+                    | Please add snapItTestDestination { <TEST SOURCESET LOCATION> } to your module build.gradle
+                    | 
+                    | Example: If test location needs to be in src/test/java/com/example/tests 
+                    | In module build.gradle add following property
+                    | snapItTestDestination {
+                    |   "com/example/tests"
+                    | }
+                    | 
+                    | Once property added please try a refresh and build
+                """.trimMargin()
+                ))
+            }
+
+            val packagePath = snapConfigFile.readText()
+
             from("build/generated/ksp/debug/kotlin/com/adbsalam/snapit/")
             into("src/test/java/$packagePath")
             filter { line -> line.replace("//", "") }
