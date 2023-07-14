937c723084fa331b4d71243b2ae3a410457e1154
Muhammad AbdulSalam <muhammadabdulsalam@Muhammads-MacBook-Pro.local>
2023-07-14 01:41:22 +0100

    working agp

--- snapit_lint/src/main/java/com/adbsalam/snapit_lint/SnapItIssueRegistry.kt
+++ snapit_lint/src/main/java/com/adbsalam/snapit_lint/SnapItIssueRegistry.kt
@@ -1,10 +1,16 @@
 package com.adbsalam.snapit_lint
 
 import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.client.api.Vendor
 import com.android.tools.lint.detector.api.CURRENT_API
 
 class SnapItIssueRegistry : IssueRegistry() {
     override val issues = listOf(SnapItIssues.ISSUE)
     override val api: Int = CURRENT_API
     override val minApi: Int = 6
+    override val vendor: Vendor = Vendor(
+        feedbackUrl = "abdulsalach32@gmail.com",
+        identifier = "com.adbsalam",
+        vendorName = "adb_salam"
+    )
 }
